d09472b2f4d85e9bd60ad4cd6ccfbe2d00abc3d3
Toyin96 <onagoruwam@gmail.com>
2021-04-18 09:27:16 +0100

    imported bootstrap to do style the page elements.

--- Learning/settings.py
+++ Learning/settings.py
@@ -34,6 +34,9 @@ INSTALLED_APPS = [
     'learning_logs',
     'users',
 
+    # third-party apps
+    'bootstrap4',
+
     # default django apps
     'django.contrib.admin',
     'django.contrib.auth',
